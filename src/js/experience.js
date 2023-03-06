aee4bfdef30b1249e48e85ebe55d7c03cd173acf
Yuliia-Kyivska <kiev.lia@gmail.com>
2023-03-06 12:53:47 +0200

    project

---
+++ src/js/experience.js
@@ -0,0 +1,50 @@
+const experience = [
+  {
+    "company": "itgid.info",
+    "city": "Ukraine",
+    "from": "2023",
+    "to": "Present",
+    "position": "Student",
+    "description": "My responsibilities included to learn Bootstrap, HTML, JavaScript, Cascading Style Sheets (CSS)"
+},
+{
+  "company": "GoIT",
+  "city": "Ukraine",
+  "from": "2022",
+  "to": "Present",
+  "position": "Student",
+  "description": "My responsibilities included to learn Trollo, Bootstrap, HTML5, React.js, JavaScript, Node.js, Git, Cascading Style Sheets (CSS), Collaboration, Communication, Leadership"
+},
+    {
+        "company": "freelance",
+        "city": "Ukraine",
+        "from": "2010",
+        "to": "2019",
+        "position": "Search Engine Optimization Analyst",
+        "description": "My responsibilities included analyzing and identifying gaps in website content, optimizing pages for improved search results, building links, and supervising content creation . I was to identify marketing opportunities and I increased website traffic. "
+    },
+    {
+        "company": "choice.ua",
+        "city": "Kyiv",
+        "from": "2013",
+        "to": "2014",
+        "position": "web-consultant SEO",
+        "description": "My responsibilities included consulting developers, holding webinars for clients of the company"
+    },
+    {
+        "company": "smartraf.ru",
+        "city": "St. Petersburg",
+        "from": "2010",
+        "to": "2014",
+        "position": "Search Engine Optimization Specialist",
+        "description": "My responsibilities included site audit, competitor analysis, reporting on internal optimization, query clustering, implementation of meta tags on the site purchase of perpetual/rental links for articles, postmen, etc. Preparation of technical requirements for the developer, copywriter. Advertising/retargeting campaigns in Google AdWords, Yandex Direct"
+    },
+    {
+      "company": "xado.com",
+      "city": "Ukraine",
+      "from": "2010",
+      "to": "2012",
+      "position": "Search Engine Optimization Analyst",
+      "description": "My responsibilities included analyzing and identifying gaps in website content, optimizing pages for improved search results, building links, and supervising content creation . I was to identify marketing opportunities and I increased website traffic. Preparation of technical requirements for the developer, copywriter. Advertising/retargeting campaigns in Google AdWords, Yandex Direct"
+  }
+]
